844673ad5cdaa682b8a52e5816300c315ce29dea
Benjamin Desef <projekter-git@yahoo.de>
2024-07-08 18:52:16 +0200

    Weakdeps for Julia < 1.9

--- src/StandardPacked.jl
+++ src/StandardPacked.jl
@@ -3,6 +3,16 @@ module StandardPacked
 
 using LinearAlgebra, SparseArrays
 
+if !isdefined(Base, :get_extension)
+    using Requires
+end
+
+@static if !isdefined(Base, :get_extension)
+    function __init__()
+        @require StaticArrays = "90137ffa-7385-5640-81b9-e52037218182" include("../ext/StandardPackedStaticArrays.jl")
+    end
+end
+
 include("spmatrix.jl")
 include("utils.jl")
 include("lapack.jl")
